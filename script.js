b4918a0f7d359803839d5186a04ff33b2713dd77
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-14 11:13:50 -0400

    Write functions to generate computer choice and retrieve human choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3);
+    if (choice === 0) {
+        return "rock";
+    }
+    else if (choice === 1) {
+        return "paper";
+    }
+    return "scissors";
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Choose rock, paper, or scissors");
+    return userInput;
+}
+
